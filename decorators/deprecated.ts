6b4a6157d86632b61fa370f8024d94b3af5a0f82
l337quez <l337.ronald@gmail.com>
2022-08-02 18:43:45 -0400

    Create deprecated.ts

---
+++ decorators/deprecated.ts
@@ -0,0 +1,15 @@
+const Deprecated = (deprecationReason: string) => {
+    return (target: any, memberName: string, propertyDescriptor: PropertyDescriptor) => {
+      // console.log({target})
+      return {
+        get() {
+          const wrapperFn = (...args: any[]) => {
+            console.warn(`Method ${ memberName } is deprecated with reason: ${ deprecationReason }`);
+            //! Llamar la función propiamente con sus argumentos
+            propertyDescriptor.value.apply(this, args); 
+          }
+          return wrapperFn;
+        }
+      }
+    }   
+}
